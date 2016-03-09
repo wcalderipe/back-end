2fe89f5f0c5cdc04e947ae1732d08f9600da7cdd
Josi Gama <josyvisa@gmail.com>
2016-03-09 09:48:18 -0300

    #30 @josigama @tayanefernandes @roberto - add test for the server

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ var server = restify.createServer({
   name: 'VamosJuntas',
 });
 
+server.get('/risks-around',function (request, response, next) {
+  response.send('');
+  next();
+})
+
 server.listen(8080, function(){
 	console.log('Hello World! :D');
 });
