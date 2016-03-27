6e82b1f58d782ade2e1c6b9d480747305447d979
Josi Gama <josyvisa@gmail.com>
2016-03-27 16:33:05 -0300

    #30 @josigama @viniciusgama - Configure mocks before making the required

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ var database = require('./db.connection');
 var Place = require('./place.model');
 
 var server = restify.createServer({
-  name: 'VamosJuntas',
+  name: 'VamosJuntas'
 });
 
 server.get('/risks-around',function (request, response) {
@@ -14,16 +14,13 @@ server.get('/risks-around',function (request, response) {
     console.log(places);
     return response.send(places);
   });
-})
-
-var start = function () {
-  console.log('a');
-  server.listen(8080, function() {
-    database.open();
-  	console.log('Hello World! :D');
-  });
-};
+});
 
 module.exports = {
-  start: start
+  start: function () {
+    server.listen(8080, function() {
+      database.open();
+    	console.log('Hello World! :D');
+    });
+  }
 };
--- tests/app_spec.js
+++ tests/app_spec.js
@@ -1,19 +1,19 @@
-var app = require('../src/app');
-
 var restify = require('restify');
 
 describe('App', function () {
   var server;
+  var app;
 
   beforeEach(function () {
     server = createSpyObj('server', ['get', 'listen']);
     spyOn(restify, 'createServer').andReturn(server);
+    app = require('../src/app');
   });
 
   describe('start', function () {
     it('starts the server', function () {
       app.start();
-  //    expect(server.listen).toHaveBeenCalled();
+      expect(server.listen).toHaveBeenCalled();
     });
   });
 });
