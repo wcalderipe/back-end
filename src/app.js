8278c179a2d25616d0f2367fc7416883d591df1f
Josi Gama <josyvisa@gmail.com>
2016-03-29 20:13:46 -0300

    #30 - @josigama @tayanefernandes - add geolocation query format

--- src/app.js
+++ src/app.js
@@ -6,15 +6,30 @@ var server = restify.createServer({
   name: 'VamosJuntas'
 });
 
+server.use(restify.queryParser());
+
 server.get('/risks-around',function (request, response) {
-  Place.find(function (err, places) {
-    if (err) {
-      return console.error(err);
-    }
-    console.log(places);
-    return response.send(places);
-  });
+  var coords = [];
+  coords[0] = request.params.longitude;
+  coords[1] = request.params.latitude;
+  console.log(coords);
+  var maxDistance = 100;
+
+  Place.find({
+        loc: {
+          $near: coords,
+          $maxDistance: maxDistance
+        }
+      }).exec(function (err, places) {
+        if (err) {
+          return console.error(err);
+        }
+        console.log(places);
+        return response.send(places);
+      });
 });
+
+
 
 module.exports = {
   start: function () {
