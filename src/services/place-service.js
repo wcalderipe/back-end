31242d2cb95406a090c06a2a8764ad941e12742b
Josi Gama <josyvisa@gmail.com>
2016-06-09 19:50:41 -0300

    #3 @josigama @roberto - Simplify place service

--- src/services/place-service.js
+++ src/services/place-service.js
@@ -2,12 +2,5 @@ var Place = require('./../domains/place.model');
 var Promise = require('q').Promise;
 
 module.exports.create = function(params) {
-  return new Promise(function(resolve, reject) {
-    Place.create(params)
-      .then(function() {
-        resolve();
-      }, function() {
-        reject();
-      });
-    });
+  return Place.create(params);
 }
--- tests/unit/services/place-service-spec.js
+++ tests/unit/services/place-service-spec.js
@@ -2,34 +2,22 @@ var Place = require('./../../../src/domains/place.model.js');
 var placeService = require('./../../../src/services/place-service.js');
 var chai = require("chai");
 var chaiAsPromised = require("chai-as-promised");
+var Promise = require('q').Promise;
 
 chai.use(chaiAsPromised);
 chai.should();
 
 describe('Place Service', function() {
-  var params = {};
-  var isPromiseSuccess;
-  var promise = {
-    then: function(success, error) {
-      if(isPromiseSuccess) {
-        success();
-      }
-      error();
-    }
-  };
-
-  beforeEach(function() {
-    isPromiseSuccess = true;
-    spyOn(Place, 'create').andReturn(promise);
-  });
+  var params = {a: 1};
 
   it('should call Place.create and creates a risk with success', function(done){
+    spyOn(Place, 'create').andReturn(Promise.resolve());
     placeService.create(params).should.be.fulfilled.and.notify(done);
     expect(Place.create).toHaveBeenCalledWith(params);
   });
 
   it('should call Place.create and reject when does not create a risk with success', function(done){
-    isPromiseSuccess = false;
+    spyOn(Place, 'create').andReturn(Promise.reject());
     placeService.create(params).should.be.rejected.and.notify(done);
     expect(Place.create).toHaveBeenCalledWith(params);
   });
